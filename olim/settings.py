d6108e7352c4441ed7b6b80418b0abcf457e0a81
abodarev <abondarev@phoenixit.ru>
2024-10-06 22:19:58 +0300

    fit 1 - решил проблему с баззой

--- olim/settings.py
+++ olim/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
-        'HOST': "localhost" if DEBUG else env('DB_HOST'),
+        'HOST': env('DB_HOST'),
         'PORT': env('DB_PORT'),
     }
 }
